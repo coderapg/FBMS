7abe26eb10f2a57c3ec4e8c0772baf48aed67506
李永峰 <1184156787@qq.com>
2022-12-30 20:29:58 +0800

    配置按需引入element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+    <el-select v-model="value" placeholder="请选择">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+      </el-option>
+    </el-select>
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      options: [
+        {
+          value: '选项1',
+          label: '黄金糕'
+        },
+        {
+          value: '选项2',
+          label: '双皮奶'
+        },
+        {
+          value: '选项3',
+          label: '蚵仔煎'
+        }
+      ],
+      value: ''
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 按需引入element-ui
+import './plugins/ele-ui'
+
 Vue.config.productionTip = false
 
 new Vue({
