3cef4d31556d9cce01c6abfca81fbf072bb7a3e9
李永峰 <1184156787@qq.com>
2023-01-01 16:48:01 +0800

    Layout页面-侧边栏导航

---
+++ src/views/layout/components/LayoutNavMenu.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-menu
+    default-active="1"
+    background-color="#002033"
+    text-color="#fff"
+    active-text-color="#ffd04b">
+    <el-menu-item index="1">
+      <i class="el-icon-s-home"></i>
+      <span slot="title">首页</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <i class="el-icon-document"></i>
+      <span slot="title">内容管理</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <i class="el-icon-picture"></i>
+      <span slot="title">素材管理</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <i class="el-icon-s-promotion"></i>
+      <span slot="title">发布文章</span>
+    </el-menu-item>
+    <el-menu-item index="5">
+      <i class="el-icon-chat-dot-round"></i>
+      <span slot="title">评论管理</span>
+    </el-menu-item>
+    <el-menu-item index="6">
+      <i class="el-icon-present"></i>
+      <span slot="title">粉丝管理</span>
+    </el-menu-item>
+    <el-menu-item index="7">
+      <i class="el-icon-setting"></i>
+      <span slot="title">个人设置</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+
+export default {
+  name: 'LayoutNavMenu'
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,6 +1,8 @@
 <template>
   <el-container class="layout">
-    <el-aside width="200px" class="aside">Aside</el-aside>
+    <el-aside width="200px" class="aside">
+      <layout-nav-menu />
+    </el-aside>
     <el-container>
       <el-header class="header">Header</el-header>
       <el-main class="main">Main</el-main>
@@ -9,16 +11,29 @@
 </template>
 
 <script>
+import LayoutNavMenu from './components/LayoutNavMenu'
 
 export default {
-  name: 'LayoutIndex'
+  name: 'LayoutIndex',
+  components: {
+    LayoutNavMenu
+  }
 }
 </script>
 
 <style lang="less" scoped>
   .layout {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
     .aside {
       background-color: #d3dce6;
+      height: 100%;
+      .el-menu {
+        height: 100%;
+      }
     }
     .header {
       background-color: #b3c0d1;
