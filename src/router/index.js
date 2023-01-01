fdf9bd256cb2d88aba97bf8d9452f68308c70e35
李永峰 <1184156787@qq.com>
2023-01-01 21:10:26 +0800

    添加路由导航守卫-用户未登录跳转到登录页面，已登录允许进行访问

--- src/router/index.js
+++ src/router/index.js
@@ -69,4 +69,22 @@ const router = new VueRouter({
   mode: 'history'
 })
 
+// 添加路由导航守卫
+router.beforeEach((to, from, next) => {
+  const userInfo = JSON.parse(window.localStorage.getItem('FBMS_User_Info'))
+  // 校验非登录页面的登录状态
+  if (to.path !== '/login') {
+    if (userInfo) {
+      // 已登录，允许通过
+      next()
+    } else {
+      // 没有登录，跳转到登录页面
+      next('/login')
+    }
+  } else {
+    // 登录页面，正常允许通过
+    next()
+  }
+})
+
 export default router
