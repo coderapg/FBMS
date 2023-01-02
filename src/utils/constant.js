552719c4bb17a96e72174e8522aa9396989c0732
李永峰 <1184156787@qq.com>
2023-01-02 09:24:39 +0800

    内容管理-优化文章状态

---
+++ src/utils/constant.js
@@ -0,0 +1,32 @@
+/**
+ * 常量配置文件
+ */
+
+// article表格状态
+export const articleStatus = [
+  {
+    status: 0,
+    text: '草稿',
+    type: 'info'
+  },
+  {
+    status: 1,
+    text: '待审核',
+    type: ''
+  },
+  {
+    status: 2,
+    text: '审核通过',
+    type: 'success'
+  },
+  {
+    status: 3,
+    text: '审核失败',
+    type: 'warning'
+  },
+  {
+    status: 4,
+    text: '已删除',
+    type: 'danger'
+  }
+]
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -53,12 +53,15 @@
         </el-table-column>
         <el-table-column prop="title" label="标题" />
         <el-table-column prop="status" label="状态">
-          <template slot-scope="scope">
+          <!-- <template slot-scope="scope">
             <el-tag type="info" v-if="scope.row.status === 0">草稿</el-tag>
             <el-tag v-else-if="scope.row.status === 1">待审核</el-tag>
             <el-tag type="success" v-else-if="scope.row.status === 2">审核通过</el-tag>
             <el-tag type="warning" v-else-if="scope.row.status === 3">审核失败</el-tag>
             <el-tag type="danger" v-else-if="scope.row.status === 4">已删除</el-tag>
+          </template> -->
+          <template slot-scope="scope">
+            <el-tag :type="articleStatus[scope.row.status].type">{{ articleStatus[scope.row.status].text }}</el-tag>
           </template>
         </el-table-column>
         <el-table-column prop="pubdate" label="发布时间" />
@@ -83,11 +86,13 @@
 
 <script>
 import { getArticles } from 'https/article'
+import { articleStatus } from 'utils/constant'
 
 export default {
   name: 'ArticleIndex',
   data () {
     return {
+      articleStatus,
       form: {
         status: null,
         channel: '',
