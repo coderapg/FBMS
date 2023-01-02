0e4501970bdb952ccefbbee9bd60197b3cc2a01f
李永峰 <1184156787@qq.com>
2023-01-02 20:54:59 +0800

    发布文章-页面布局

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,12 +1,71 @@
 <template>
-  <div>发布文章</div>
+  <el-card class="screen">
+    <!-- 面包屑导航 -->
+    <div slot="header" class="clearfix">
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>发布文章</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <!-- /面包屑导航 -->
+    <el-form ref="form" :model="form" label-width="60px">
+      <el-form-item label="标题">
+        <el-input v-model="form.title"></el-input>
+      </el-form-item>
+      <el-form-item label="频道">
+        <el-select v-model="form.channel_id" placeholder="请选择频道">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="内容">
+        <el-input type="textarea" v-model="form.content"></el-input>
+      </el-form-item>
+      <el-form-item label="封面">
+        <el-radio-group v-model="form.cover.type">
+          <el-radio :label="1">单图</el-radio>
+          <el-radio :label="3">三图</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handleOnPublish">发布</el-button>
+        <el-button>存入草稿</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script>
 
 export default {
-  name: 'PublishIndex'
+  name: 'PublishIndex',
+  data () {
+    return {
+      form: {
+        channel_id: null,
+        content: '',
+        cover: {
+          images: [],
+          type: 1
+        },
+        title: ''
+      }
+    }
+  },
+  methods: {
+    handleOnPublish () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+  .el-form {
+    /deep/ .el-form-item__content {
+      width: 400px;
+    }
+  }
+</style>
