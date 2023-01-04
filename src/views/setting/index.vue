29854407528b59eaf7e44baab3e2de94b366cc4d
李永峰 <1184156787@qq.com>
2023-01-04 14:29:04 +0800

    个人设置-表单验证

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -11,17 +11,17 @@
     <!-- 表单 -->
     <el-row :gutter="10">
       <el-col :xs="12" :sm="12" :md="12" :lg="12" :xl="12">
-        <el-form ref="form" :model="form" label-width="80px">
+        <el-form ref="form" :model="form" :rules="rules" label-width="80px">
           <el-form-item label="编号">123</el-form-item>
           <el-form-item label="手机号">17860143911</el-form-item>
-          <el-form-item label="媒体名称">
-            <el-input v-model="form.name"></el-input>
+          <el-form-item label="邮箱" prop="email">
+            <el-input placeholder="请输入邮箱地址" v-model="form.email"></el-input>
           </el-form-item>
-          <el-form-item label="媒体介绍">
-            <el-input type="textarea" v-model="form.intro"></el-input>
+          <el-form-item label="媒体名称" prop="name">
+            <el-input placeholder="请输入媒体名称" v-model="form.name"></el-input>
           </el-form-item>
-          <el-form-item label="邮箱">
-            <el-input v-model="form.email"></el-input>
+          <el-form-item label="媒体介绍" prop="intro">
+            <el-input type="textarea" placeholder="请输入媒体介绍" v-model="form.intro" />
           </el-form-item>
           <el-form-item>
             <el-button type="primary" @click="onSubmit">保存设置</el-button>
@@ -50,6 +50,20 @@ export default {
         name: '',
         intro: '',
         email: ''
+      },
+      rules: {
+        email: [
+          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+          { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+        ],
+        name: [
+          { required: true, message: '请输入媒体名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        intro: [
+          { required: true, message: '请输入媒体名称', trigger: 'blur' },
+          { min: 5, max: 300, message: '长度在 5 到 300 个字符', trigger: 'blur' }
+        ]
       },
       circleUrl: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
     }
